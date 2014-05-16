eb79a815ff07ce3481e6b6e99e5b96f2cd2f5a2b
Greg McCabe <gsmccabe1@gmail.com>
2014-05-16 15:38:37 -0500

    todo2 list

---
+++ todo2.php
@@ -0,0 +1,53 @@
+<?php
+
+
+$items = array('');
+unset($items[0]);
+
+function list_items($list){
+    $result = '';
+  foreach ($list as $key => $value) {
+        $result .= "[$key] $value" . PHP_EOL ;
+  }
+  return $result; 
+}
+
+function get_input($upper = false) {
+    $results = trim(fgets(STDIN));
+    return $upper ? strtoupper($results) : $results;
+}
+
+do {
+    
+    // Echo the list produced by the function
+    echo list_items($items);
+
+    // Show the menu options
+    echo '(N)ew item, (R)emove item, (Q)uit : ';
+
+    // Get the input from user
+    // Use trim() to remove whitespace and newlines
+    $input = get_input(true);
+
+    // Check for actionable input
+    if ($input == 'N') {
+        // Ask for entry
+        echo 'Enter item: ';
+        // Add entry to list array
+        $items[] = get_input();
+    } elseif ($input == 'R') {
+        // Remove which item?
+        echo 'Enter item number to remove: ';
+        // Get array key
+        $key = get_input();
+        // Remove from array
+        unset($items[$key]);
+    }
+// Exit when input is (Q)uit
+} while ($input != 'Q');
+
+// Say Goodbye!
+echo "Goodbye!\n";
+
+// Exit with 0 errors
+exit(0);
